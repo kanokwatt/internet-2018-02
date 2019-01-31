7aa734a1b12da7a6202c3b8b4cf5fae2e4b15f4c
kaewr <32901197+kaewr@users.noreply.github.com>
2019-01-31 12:03:17 +0700

    modified with status, hobby

--- show.php
+++ show.php
@@ -1,9 +1,19 @@
 <?php
 
-    $fname = $_REQUEST['fname'];
-    $lname = $_REQUEST['lname'];
+    // initialize
+    $book = "";
+    $game = "";
+
+    $fname  = $_REQUEST['fname'];
+    $lname  = $_REQUEST['lname'];
     $gender = $_REQUEST['gender'];
+    $status = $_REQUEST['status'];
+
+    if(isset($_REQUEST['book'])) $book = $_REQUEST['book'];
+    if(isset($_REQUEST['game'])) $game = $_REQUEST['game'];      
 
     echo "Hi, " . $fname . "   " . $lname . "<br>" ;
-    echo "Gender: " . $gender;
+    echo "Gender: " . $gender . "<br>";
+    echo "Status: " . $status . "<br>" ;
+    echo "Hobby: " . $game . ", " . $book;
 ?>
