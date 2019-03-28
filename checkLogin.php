0cbfc5af383758521b26112f2655eb700798e5bc
kaewr <32901197+kaewr@users.noreply.github.com>
2019-03-28 11:57:53 +0700

    Basic login

---
+++ checkLogin.php
@@ -0,0 +1,32 @@
+<?php
+// checkLogin.php
+
+/*************************/
+// STEP 1: CONNECT
+/*************************/
+require("connectDB.php");
+
+$user = $_REQUEST["user"];
+$pwd = $_REQUEST["pwd"];
+
+/*************************/
+// STEP 2: SELECT (SQL)
+/*************************/
+$sql = "SELECT * FROM student WHERE STUDENT_ID=". $user . " AND PASSWORD='" . $pwd ."'";
+//echo $sql;
+
+/*************************/
+// STEP 3: EXECUTE (SQL)
+/*************************/
+$result = mysqli_query($conn, $sql); 
+
+if (mysqli_num_rows($result) > 0) {
+    //echo "Login OK!";
+    header("Location: student.php");
+}
+else {
+    //echo "Login Failed!";
+    header("Location: login.php?status=-1");
+}
+
+?>
--- login.php
+++ login.php
@@ -1,6 +1,18 @@
+<?php
+  $status = 0;
+
+  if (isset($_REQUEST['status'])) {
+      $status = $_REQUEST['status'];
+  }
+?>
 <html>
     <head>เข้าใช้งานระบบ</head>
     <body>
+    <?php
+      if ($status == -1) {
+          echo "<h3>Incorrect username or password!</h3>";
+      }
+    ?>
         <form action="checkLogin.php">
             Username: <input type="text" name="user"><br>
             Password: <input type="text" name="pwd"><br>
