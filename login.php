81822058f8fb471695f4017a4be12f5a3e996724
kaewr <32901197+kaewr@users.noreply.github.com>
2019-03-28 11:09:52 +0700

    basic login

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+    <head>เข้าใช้งานระบบ</head>
+    <body>
+        <form action="checkLogin.php">
+            Username: <input type="text" name="user"><br>
+            Password: <input type="text" name="pwd"><br>
+            <input type="submit" value="Login">
+
+        </form>
+    </body>
+</html>
